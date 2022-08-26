344353ec9db59f7e9054faecdc6bba71f9fb6307
FiVo <finn.volkel@gmail.com>
2022-08-26 17:21:27 +0200

    sketch of min-count-sketch

---
+++ core/src/xtdb/min_count_sketch.clj
@@ -0,0 +1,55 @@
+(ns xtdb.min-count-sketch
+  (:require [xtdb.memory :as mem]
+            [bigml.sketchy.murmur :as murmur])
+  (:import [org.agrona DirectBuffer MutableDirectBuffer]
+           [org.agrona.concurrent AtomicBuffer]))
+
+(def ^:private offset (+ Short/BYTES Byte/BYTES))
+(defn size [w d] (+ offset (* w d Long/BYTES)))
+
+(defn create [{:keys [w d] :or {w 15 d 3}}]
+  (let [s (size w d)]
+    (doto ^MutableDirectBuffer (mem/allocate-buffer s)
+      (.putShort 0 (short w))
+      (.putByte Short/BYTES (byte d))
+      (.setMemory offset (- s offset) 0))))
+
+(defn- highest-bit [w]
+  (loop [i 0]
+    (if (zero? (bit-shift-right w i))
+      i
+      (recur (inc i)))))
+
+(defn insert ^MutableDirectBuffer [^AtomicBuffer sketch val]
+  (let [w (.getShortVolatile sketch 0)
+        d (.getByte sketch Short/BYTES)]
+    (doseq [j (range d)]
+      (let [i (mod (murmur/hash val j) w)
+            #_(murmur/truncate (murmur/hash val j) (highest-bit w))]
+        (doto sketch
+          (.getAndAddLong (+ offset (* j i Long/BYTES)) 1))))
+    sketch))
+
+(defn estimate ^long [^AtomicBuffer sketch val]
+  (let [w (.getShortVolatile sketch 0)
+        d (.getByte sketch Short/BYTES)
+        res (->> (for [j (range d)]
+                   (let [i (mod (murmur/hash val j) w)
+                         #_(murmur/truncate (murmur/hash val j) (highest-bit w))]
+                     (.getLongVolatile sketch (+ offset (* j i Long/BYTES)))))
+                 #_(remove zero?))]
+    (if (seq res)
+      (reduce min res)
+      0)))
+
+(comment
+  (def sktch (create {:w 256 :d 5}))
+
+  (insert sktch :foo/bar)
+  (estimate sktch :foo/bar)
+  (estimate sktch :foo/foo)
+
+
+
+
+  )
