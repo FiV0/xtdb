9f64c411f52e57c5af48ea402e1f389710600472
FiVo <finn.volkel@gmail.com>
2022-08-31 15:32:00 +0200

    [squash-me] before stats-tests

--- test/test/xtdb/kv/index_store_test.clj
+++ test/test/xtdb/kv/index_store_test.clj
@@ -240,15 +240,20 @@
                                          {:doc-count (db/doc-count index-snapshot attr)
                                           :doc-value-count (db/doc-value-count index-snapshot attr)
                                           :values (Math/round (db/value-cardinality index-snapshot attr))
-                                          :eids (Math/round (db/eid-cardinality index-snapshot attr))
-                                          :value-counts
-                                          (->>
-                                           (db/av index-snapshot attr mem/empty-buffer)
-                                           (map c/decode-value-buffer)
-                                           (map (juxt identity
-                                                      (fn [value]
-                                                        (Math/round (db/attr-value-cardinality index-snapshot attr value)))))
-                                           (into {}))})))))))]
+                                          :eids (Math/round (db/eid-cardinality index-snapshot attr))})))))))
+          (->attr-value-stats [index-snapshot-factory]
+            (with-open [index-snapshot (db/open-index-snapshot index-snapshot-factory)]
+              (->> (db/all-attrs index-snapshot)
+                   (map (juxt identity
+                              (fn [attr]
+                                (let [vals (->> (db/av index-snapshot attr mem/empty-buffer)
+                                                (map c/decode-value-buffer))]
+                                  (->> vals
+                                       (map (juxt identity
+                                                  (fn [value]
+                                                    (Math/round (db/attr-value-cardinality index-snapshot attr value)))))
+                                       (into {}))))))
+                   (into {}))))]
     (with-fresh-index-store
       (let [ivan {:crux.db/id :ivan, :name "Ivan", :interests #{:clojure :databases}}
             ivan2 {:crux.db/id :ivan, :name "Ivan2", :interests #{:clojure :databases :bitemporality}}
@@ -269,12 +274,8 @@
             (db/index-tx #::xt{:tx-time #inst "2022", :tx-id 1})
             (index-docs {(c/new-id ivan2) ivan2}))
 
-          (t/is (= {:doc-count 3, :doc-value-count 3, :values 3, :eids 2
-                    :value-counts {"Ivan" 1 "Ivan2" 1 "Petr" 1}}
-                   (:name (->stats *index-store*))))
-          (t/is (= {:doc-count 2, :doc-value-count 5, :values 3, :eids 1
-                    :value-counts {:clojure 2 :databases 2 :bitemporality 1}}
-                   (:interests (->stats *index-store*)))))
+          (t/is (= {:doc-count 3, :doc-value-count 3, :values 3, :eids 2} (:name (->stats *index-store*))))
+          (t/is (= {:doc-count 2, :doc-value-count 5, :values 3, :eids 1} (:interests (->stats *index-store*)))))
 
         (t/testing "duplicate docs are reflected twice in the doc-count"
           ;; this isn't ideal, but stats won't ever be 100% accurate
@@ -286,8 +287,7 @@
                      (:name (->stats *index-store*))))))))
 
     (with-fresh-index-store
-      (let [id-iterator (.iterator ^Iterable (range))
-            n-docs 50]
+      (let [id-iterator (.iterator ^Iterable (range))]
         (letfn [(mk-docs [n]
                   (for [idx (range n)
                         sub-idx (range idx)]
@@ -325,7 +325,7 @@
                  (update :sub-idx dissoc :value-counts))))))
 
 (comment
-  (clojure.test/test-vars [#'test-statistics]))
+  (clojure.test/test-var #'test-statistics))
 
 (t/deftest test-entity
   (with-fresh-index-store
