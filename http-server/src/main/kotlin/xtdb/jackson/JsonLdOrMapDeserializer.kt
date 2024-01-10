382b0e4df995fe4d955dabcea5a2bc7ef654800a
Finn Völkel <finn.volkel@gmail.com>
2024-01-10 16:40:57 +0100

    wip

--- http-server/src/main/kotlin/xtdb/jackson/JsonLdOrMapDeserializer.kt
+++ http-server/src/main/kotlin/xtdb/jackson/JsonLdOrMapDeserializer.kt
@@ -1,13 +1,45 @@
 package xtdb.jackson
 
-import kotlinx.serialization.json.JsonContentPolymorphicSerializer
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.element
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.*
+import java.time.Instant
 
+object InstantSerializer : KSerializer<Instant> {
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("JsonLd") {
+        element<String>("@type")
+        element<String>("@value")
+    }
+
+    override fun serialize(encoder: Encoder, value: Instant) {
+        require(encoder is JsonEncoder)
+        encoder.encodeJsonElement(buildJsonObject {
+            put("@type", "xt:instant")
+            put("@value", value.toString())
+        })
+    }
+
+    override fun deserialize(decoder: Decoder): Instant {
+        require(decoder is JsonDecoder)
+        val jsonObject = decoder.decodeJsonElement()
+
+        if (jsonObject is JsonObject) {
+            val type = jsonObject["@type"]
+            return Instant.parse(jsonObject["@value"]!!.jsonPrimitive.toString())
+        } else {
+            TODO()
+        }
+    }
+
+
+}
 object JsonLdOrMapDeserializer: JsonContentPolymorphicSerializer<Any>(Any::class) {
     override fun selectDeserializer(element: JsonElement) = when {
-        "@type" in element.jsonObject -> JsonLdSerializer()
+        "@type" in element.jsonObject -> InstantSerializer
         else -> JsonObject.serializer()
     }
 }
--- http-server/src/test/kotlin/xtdb/jackson/JsonLdOrMapDeserializerTest.kt
+++ http-server/src/test/kotlin/xtdb/jackson/JsonLdOrMapDeserializerTest.kt
@@ -1,9 +1,9 @@
 package xtdb.jackson
 
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.time.Instant
@@ -11,8 +11,8 @@ import java.time.Instant
 
 val json = Json { serializersModule
      SerializersModule {
-         contextual(Map::class, JsonLdOrMapDeserializer as KSerializer<Map<*,*>>)
-         contextual(Instant::class, JsonLdOrMapDeserializer as KSerializer<Instant>)
+         contextual(Any::class, JsonLdOrMapDeserializer)
+         polymorphic(Instant::class, JsonLdOrMapDeserializer as KSerializer<Instant>)
     }
 }
 
@@ -39,20 +39,20 @@ class JsonLdOrMapDeserializerTest {
 
 
 
-    @Test
-    fun testJsonLdInstantSerialization() {
-        // Arrange
-        val expected =
-            """
-            { "@type":"xt:instant", "@value":"2020-01-01T00:00:00Z" }
-            """.trimIndent()
-
-        // Act
-        val actual = json.encodeToString(Instant.parse("2020-01-01T00:00:00Z"))
-
-        // Assert
-        Assertions.assertEquals(expected, actual)
-    }
+//    @Test
+//    fun testJsonLdInstantSerialization() {
+//        // Arrange
+//        val expected =
+//            """
+//            { "@type":"xt:instant", "@value":"2020-01-01T00:00:00Z" }
+//            """.trimIndent()
+//
+//        // Act
+//        val actual = json.encodeToString(Instant.parse("2020-01-01T00:00:00Z"))
+//
+//        // Assert
+//        Assertions.assertEquals(expected, actual)
+//    }
 
 
 }
