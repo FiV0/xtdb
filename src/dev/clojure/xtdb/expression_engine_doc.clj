6ea810088e7db4e6cc390df3235a765ca635ab87
Finn Völkel <finn.volkel@gmail.com>
2024-03-21 17:02:33 +0100

    wip

--- src/dev/clojure/xtdb/expression_engine_doc.clj
+++ src/dev/clojure/xtdb/expression_engine_doc.clj
@@ -42,6 +42,27 @@
 
 (defn ->vec-wrt [] (->NullableLongVectorWriter []))
 
+;; here I just implemented ValueBox as something implementing IVectorReader/IVectorWriter
+;; In our real usage this implements ValueReader, but I didn't want to blow this implementation
+;; out of proportion
+
+(deftype ValueBox [^:unsynchronized-mutable value]
+  IVectorReader
+  (isNull [_ _] (nil? value))
+  (getLong [_ _] (if value
+                   value
+                   (throw (UnsupportedOperationException.))))
+
+  IVectorWriter
+  (writeNull [_] (set! value nil))
+  (writeLong [_ lng] (set! value lng))
+
+  Object
+  (toString [_]
+    (format "NullableLongValueBox[%s]" (pr-str value))))
+
+(defn ->value-box [] (->ValueBox nil))
+
 (comment
   (def vec-rdr (->vec-rdr [1 nil 2]))
   (.isNull vec-rdr 0)
@@ -50,10 +71,16 @@
   (def vec-wrt (->vec-wrt))
   (.writeNull vec-wrt)
   (.writeLong vec-wrt 1)
-  vec-wrt)
+  vec-wrt
+
+  (def value-box (->value-box))
+  (.isNull value-box -1)
+  (.writeLong value-box 42)
+  (.getLong value-box -1))
 
 
 ;; the language
+;; we consider nil to be false
 (comment
   nil
   1
@@ -129,3 +156,61 @@
   (-> (parse-expr '(let [x (if (+ 1 2) 3 4)]
                      (+ 1 2 3)))
       (invoke {})))
+
+
+;; second approach direct compiler
+
+(def idx-sym (gensym 'idx))
+
+(defmulti codegen-direct (fn [expr] (cond (nil? expr) :nil
+                                          (number? expr) :long
+                                          (symbol? expr) :var
+                                          (list? expr) (keyword (first expr)))))
+
+(defmethod codegen-direct :nil [_] nil)
+(defmethod codegen-direct :long [lng] lng)
+(defmethod codegen-direct :var [var]
+  `(when-not (.isNull ~var ~idx-sym)
+     (.getLong ~var ~idx-sym)))
+
+(defmethod codegen-direct :+ [[_ x-expr y-expr]]
+  `(if-let [x-res# ~(codegen-direct x-expr)]
+     (if-let [y-res# ~(codegen-direct y-expr)]
+       (Math/addExact x-res# y-res#)
+       nil)
+     nil))
+
+(defmethod codegen-direct :if [[_ cond if-branch else-branch]]
+  `(if ~(codegen-direct cond)
+     ~(codegen-direct if-branch)
+     ~(codegen-direct else-branch)))
+
+(defmethod codegen-direct :let [[_ [binding b-expr] body]]
+  `(let [~binding (->value-box)]
+     (if-let [b-expr-res# ~(codegen-direct b-expr)]
+       (.writeLong ~binding b-expr-res#))
+     ~(codegen-direct body)))
+
+(comment
+  (codegen-direct '(if 1 (+ 1 1) 3))
+  (codegen-direct '(if (+ 1 2) 3 4))
+  (codegen-direct '(let [x (if (+ 1 2) 3 4)]
+                     (+ 1 x))))
+
+(defn compile-expr [expr col-names]
+  (-> `(fn ~(vec col-names)
+         (let [res-vec# (->vec-wrt)]
+           (dotimes [~idx-sym 3]
+             (if-let [res# ~(codegen-direct expr)]
+               (.writeLong res-vec# res#)
+               (.writeNull res-vec#)))
+           res-vec#))
+      eval))
+
+(comment
+  ((compile-expr '(+ x 1) ['x]) (->vec-rdr [1 nil 2]))
+
+  ((compile-expr '(let [x (if (+ 1 y) 10 1)]
+                    (+ 1 x))
+                 ['y])
+   (->vec-rdr [1 nil 2])))
